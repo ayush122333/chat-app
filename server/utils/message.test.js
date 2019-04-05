8431fe4f7fff9e236f3c453c7a2b5724b6e68b1d
ayush <kumarayush197@gmail.com>
2019-04-05 18:23:21 +0530

    new generateMessage

---
+++ server/utils/message.test.js
@@ -0,0 +1,27 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'ayush';
+        var text = 'hello sir';
+        var message = generateMessage(from, text);
+        
+        
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
